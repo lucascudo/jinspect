7983e7de35dddd0353cb6967194af38c690140d4
cascudo <lucas.sisau@gmail.com>
2019-10-09 22:35:21 -0300

    handle json request

--- src/main/java/jinspect/SpotCheckController.java
+++ src/main/java/jinspect/SpotCheckController.java
@@ -1,13 +1,15 @@
 package jinspect;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpotCheckController {
 
-    @RequestMapping("/spot_check")
-    public SpotCheck spotcheck(@RequestParam(value = "plate", defaultValue = " ") String plate) {
-        //return new SpotCheck(plate);
+    @RequestMapping(value = "/spot_check", produces = "application/json")
+    public @ResponseBody SpotCheck spotcheck(@RequestBody Boolean[][] plate) {
+        return new SpotCheck(plate);
     }
 }
