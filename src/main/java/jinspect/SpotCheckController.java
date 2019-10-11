7d411a32bedeff55cc84641ae2e9cb34ce78de8e
Lucas Rocha <lucas.rocha@cworks.com.br>
2019-10-11 17:29:05 -0300

    adds matrix validation

--- src/main/java/jinspect/SpotCheckController.java
+++ src/main/java/jinspect/SpotCheckController.java
@@ -1,6 +1,7 @@
 package jinspect;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpotCheckController {
 
-    @RequestMapping(value = "/spot_check", produces = "application/json")
-    public @ResponseBody SpotCheck spotcheck(@RequestBody Boolean[][] plate) {
+    @RequestMapping(value = "/spot_check", method = RequestMethod.POST)
+    public @ResponseBody Object spotCheck(@RequestBody Boolean[][] plate) {
+        Integer plateRows = plate.length; 
+        for (Boolean[] row: plate) {
+            if (row.length != plate.length) {
+                return "Invalid Matrix Format";
+            }    
+        }
         return new SpotCheck(plate);
+    }
+
+    @RequestMapping(value = "/spot_check", method = RequestMethod.GET)
+    public @ResponseBody String spotCheckInvalidMethod(@RequestBody Boolean[][] plate) {
+        return "Invalid Method";
     }
 }
